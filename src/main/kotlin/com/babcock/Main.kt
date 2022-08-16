033b41667a93acbb0b3344b24a9b9a8e1cddb7cf
drewb8L <104442990+drewb8L@users.noreply.github.com>
2022-08-16 13:34:56 -0500

    initial commit

---
+++ src/main/kotlin/com/babcock/Main.kt
@@ -0,0 +1,14 @@
+package com.babcock
+
+class Main {
+    fun mainTest():Int {
+        return 1
+    }
+companion object{
+    @JvmStatic  fun main(args: Array<String>){
+        println("Hello server")
+    }
+
+}
+
+}
---
+++ src/test/kotlin/mainTest.kt
@@ -0,0 +1,12 @@
+import com.babcock.Main
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Test{
+    @Test
+    fun testMain(){
+        val main = Main()
+
+        assertEquals(main.mainTest(), 1)
+    }
+}

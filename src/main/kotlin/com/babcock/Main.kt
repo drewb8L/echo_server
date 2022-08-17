36b7a9df3aeabd1964d5361d0c600bae6ac45823
drewb8L <104442990+drewb8L@users.noreply.github.com>
2022-08-17 12:06:52 -0500

    Created server socket test and main function

--- src/main/kotlin/com/babcock/Main.kt
+++ src/main/kotlin/com/babcock/Main.kt
@@ -1,14 +1,20 @@
 package com.babcock
 
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import kotlinx.coroutines.Dispatchers
+
 class Main {
     fun mainTest():Int {
         return 1
     }
 companion object{
     @JvmStatic  fun main(args: Array<String>){
-        println("Hello server")
-    }
+        val sm = SelectorManager(Dispatchers.IO)
+        val aSocket = aSocket(sm).tcp().bind("127.0.0.1", args[0].toInt())
+        println("Server is listening at ${aSocket.localAddress}, Context: ${aSocket.socketContext}")
 
+    }
 }
 
 }
--- src/test/kotlin/mainTest.kt
+++ src/test/kotlin/mainTest.kt
@@ -1,11 +1,27 @@
 import com.babcock.Main
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import kotlinx.coroutines.Dispatchers
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import java.io.IOException
+import kotlin.jvm.Throws
 import kotlin.test.assertEquals
+
 
 class Test{
     @Test
     fun testMain(){
         val main = Main()
         assertEquals(main.mainTest(), 1)
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun serverSocket() {
+        val selectorManager = SelectorManager(Dispatchers.Default)
+        val serverSocket = aSocket(selectorManager)
+        assertNotNull(serverSocket)
+
     }
 }

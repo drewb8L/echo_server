14fb9173aa894642b609ca2aad5b6b5b66264aa0
drewb8L <104442990+drewb8L@users.noreply.github.com>
2022-08-17 12:57:46 -0500

    Omitting kotlinc from ci.yml

--- src/main/kotlin/com/babcock/Main.kt
+++ src/main/kotlin/com/babcock/Main.kt
@@ -3,18 +3,25 @@ package com.babcock
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+
+
 
 class Main {
-    fun mainTest():Int {
+    fun mainTest(): Int {
         return 1
     }
-companion object{
-    @JvmStatic  fun main(args: Array<String>){
-        val sm = SelectorManager(Dispatchers.IO)
-        val aSocket = aSocket(sm).tcp().bind("127.0.0.1", args[0].toInt())
-        println("Server is listening at ${aSocket.localAddress}, Context: ${aSocket.socketContext}")
 
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Echo Server")
+            val sm = SelectorManager(Dispatchers.IO)
+            val aSocket = aSocket(sm).tcp().bind("127.0.0.1", 8080)
+            println("Server is listening at ${aSocket.localAddress}, Context: ${aSocket.socketContext}")
+            aSocket.close()
+
+        }
     }
-}
 
 }
